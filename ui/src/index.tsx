86c88e9587246e4ba4c1bd5db612e550ecbc6873
TemplarFelix <contato@templarfelix.com>
2023-08-01 19:00:53 -0300

    debug

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,19 +1,19 @@
 import * as React from 'react';
 
-export const Extension = (props: any) => { 
-return (
-  <div>
-    Hello {props}!
-  </div>
+export const Extension = (props: any) => {
   
-)};
+
+  return (
+    <div>hello</div>
+  );
+};
 
 export const component = Extension;
 
 ((window: any) => {
    window.extensionsAPI.registerAppViewExtension(
     component,
-    "Test Ext",
+    "Argo Rollouts",
     "fa-flask"
   );
 })(window);
